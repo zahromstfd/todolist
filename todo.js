321023c75c18ce9f7e170e853b786e5b624ad28f
zahromstfd <zahromustafidah27@gmail.com>
2022-10-16 17:13:35 +0700

    latihan membuat website to do list menggunakan html css javascript

---
+++ todo.js
@@ -0,0 +1,32 @@
+var data = [];
+
+function tampil(){
+    var tabel = document.getElementById("tabel");
+    tabel.innerHTML = "<tr><th>No</th><th>Task</th><th>Action</th></tr>";
+    for (let i = 0; i < data.length; i++) {
+        var btnEdit = "<button class='btn-edit' href='#' onclick='edit(" + i + ")'>Edit</button>";
+        var btnHapus = "<button class='btn-hapus' href='#' onclick='hapus(" + i + ")'>Delete</button>";
+        j = i + 1;
+        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + data[i] + "</td><td>" + btnEdit + " " + btnHapus + "</tr>";
+    }
+}
+
+function tambah() {
+    var input = document.querySelector("input[name=task]");
+    data.push(input.value);
+    tampil();
+    input.value = "";
+}
+
+function edit(id) {
+    var baru = prompt("Edit", data[id]);
+    data[id] = baru;
+    tampil();
+}
+
+function hapus(id) {
+    data.pop(id);
+    tampil();
+}
+
+tampil();
